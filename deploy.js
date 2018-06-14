754f24650cc87fe58e3fa1cdcdd4cfd1736fbea4
Phillip Dodd <phillipdodd@gmail.com>
2018-06-14 19:32:14 -0400

    removed try/catch blocks

--- deploy.js
+++ deploy.js
@@ -10,21 +10,14 @@ const provider = new HDWalletProvider(
 
 const web3 = new Web3(provider);
 const deploy = async () => {
-    try {
-        const accounts = await web3.eth.getAccounts();
-    } catch (e) {
-        console.log(e);
-    }
+
+    const accounts = await web3.eth.getAccounts();
+
     console.log('Attempting to deploy from account ' + accounts[0]);
 
-    try {
-        const result = await new web4.eth.Contract(JSON.parse(interface))
-            .deploy({ data: bytecode })
-            .send({ gas: '1000000', from: accounts[0] });
-    } catch (e) {
-        console.log(e)
-    }
-    
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode })
+        .send({ gas: '2000000', from: accounts[0] });
 
     console.log(interface);
     console.log('Contract deployed to ', result.options.address);
